d966ca6d3b002cd3954f3ef399b4373ff08d78e5
huesos <bonirosales@gmail.com>
2018-10-29 23:16:33 -0500

    routes and gitignore

--- server.js
+++ server.js
@@ -20,8 +20,13 @@ app.use(express.json());
 
 // ROUTES
 // =============================================
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
 
-
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+});
 
 
 // START LISTENING
